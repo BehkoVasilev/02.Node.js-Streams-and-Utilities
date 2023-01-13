55e78aa6473bdf2f80a963f01ffb1f4b70da02e7
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-13 21:20:30 +0200

    reading file

---
+++ fs/readFile.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const fsp = require('fs/promises');
+
+//Synchronous reading
+const text = fs.readFileSync('./fs/text.txt');
+console.log(text.toString());
+console.log("Read from file");
+
+//Asynchronous reading 
+fs.readFile('./fs/text.txt', (err, data) => {
+    if (err) {
+        return;
+    }
+
+    console.log(data.toString());
+});
+console.log("Read from file");
+
+//Asynchronous reading with promises
+fsp.readFile('./fs/text.txt', {encoding: 'utf-8'})
+    .then(result => {
+        console.log(result);
+    })
+console.log("Read from file");
